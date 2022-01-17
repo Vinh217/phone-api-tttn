d1f68289af8bc803ee3b7de2f77254e57fd28192
Đỗ Quang Vinh <vinhdq@hblab.vn>
2022-01-17 23:18:07 +0700

    udpate

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const queryString = require('query-string')
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -16,15 +17,36 @@ server.get('/echo', (req, res) => {
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-    req.body.updatedAt = Date.now()
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
   }
   // Continue to JSON Server router
   next()
 })
+router.render = (req,res) => {
+  const headers = res.getHeaders();
 
+  const totalCountHeader = headers['x-total-count'];
+  if(req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req['_parsedUrl'].query);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader)
+      }
+    };
+
+    return res.jsonp(result);
+  }
+
+  res.jsonp(res.locals.data);
+}
 // Use default router
-server.use(router)
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.use('/api',router);
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
